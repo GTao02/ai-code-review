95010e8e8b5a8d2e4373d9fb131e053441ed7cfa
gongtao <2662187002@qq.com>
2025-03-12 22:49:33 +0800

    test: 解析git分支变更

--- ai-code-review-trigger/src/main/java/com/tao/trigger/http/GitEventController.java
+++ ai-code-review-trigger/src/main/java/com/tao/trigger/http/GitEventController.java
@@ -1,5 +1,7 @@
 package com.tao.trigger.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,9 @@ public class GitEventController {
     @RequestMapping(value = "/event", method = RequestMethod.POST)
     public void event(@RequestBody String event) {
         System.out.println("event:" + event);
+        JSONObject jsonObject = JSON.parseObject(event);
+        String before = (String) jsonObject.get("before");
+        String after = (String) jsonObject.get("after");
+        System.out.println("before:" + before + "\nafter:" + after);
     }
-
 }
